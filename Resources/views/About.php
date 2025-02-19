f802567505f01d483141b0598e92ffaea8683b54
cjaylao <cjaylao0327@gmail.com>
2025-02-19 11:48:51 +0800

    first commit

---
+++ Resources/views/About.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    About Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro consequatur illo dolores eos odit exercitationem eum illum, obcaecati iste, molestiae reprehenderit accusamus tempore ad! Quidem nesciunt incidunt voluptatum provident necessitatibus.
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam quasi voluptatum recusandae fugit expedita molestias rerum laborum repudiandae et praesentium dolorem rem voluptates, voluptate esse asperiores adipisci eum. Illum, quidem!
+    
+    About Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro consequatur illo dolores eos odit exercitationem eum illum, obcaecati iste, molestiae reprehenderit accusamus tempore ad! Quidem nesciunt incidunt voluptatum provident necessitatibus.
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam quasi voluptatum recusandae fugit expedita molestias rerum laborum repudiandae et praesentium dolorem rem voluptates, voluptate esse asperiores adipisci eum. Illum, quidem!
+    
+    About Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro consequatur illo dolores eos odit exercitationem eum illum, obcaecati iste, molestiae reprehenderit accusamus tempore ad! Quidem nesciunt incidunt voluptatum provident necessitatibus.
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam quasi voluptatum recusandae fugit expedita molestias rerum laborum repudiandae et praesentium dolorem rem voluptates, voluptate esse asperiores adipisci eum. Illum, quidem!
+</body>
+</html>
---
+++ Resources/views/Cart.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil cumque et veniam. Nemo rerum ab, accusamus rem, quam ex minima impedit excepturi, distinctio iusto modi similique enim reiciendis. Quod, nihil.
+</body>
+</html>
